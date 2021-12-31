029e22204b17ff9b3217820daff99f132e7b6fc4
andrey12v <andrey12v@gmail.com>
2021-12-30 20:31:11 -0600

    Replace inline dynamic styles with inline dynamic classes in CourseInput component
    1) Replaced inline dynamic styles with inline dynamic classes in the CourseInput component
    2) Install styled-components package and applied it for Button using its special style
    https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/25597264#announcements

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -25,12 +25,14 @@ const CourseInput = props => {
     props.onAddGoal(enteredValue);
   };
 
+  //`` backtick symbol is reserved symbol in java script which treats any text as a regular string 
+  // you can also inject dynamic value into this string with the specail style ${}
+  // in our example we add style invalid if form control is invalid 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
-        <input style={{borderColor: !isValid ? 'red' : '#ccc', 
-          background: !isValid ? 'salmon' : 'transparent'}} type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label >Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
